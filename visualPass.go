251ddba16b6cbc25a72494f03ee01247d5dcdf80
Harichand Manoj <harichandmanoj@gmail.com>
2024-11-11 22:25:53 +0530

    added magnitude

--- visualPass.go
+++ visualPass.go
@@ -23,6 +23,7 @@ type getPass struct {
 	StartEl     float64 `json:"maxEl"`
 	EndAz       float64 `json:"endAz"`
 	EndAzComp   string  `json:"endAzCompass"`
+	Mag         float32 `json:"mag"`
 }
 
 func visualPass() {
@@ -33,7 +34,7 @@ func visualPass() {
 	apiKey := setKey()
 	encoded := url.QueryEscape(apiKey)
 
-	url := fmt.Sprintf("https://api.n2yo.com/rest/v1/satellite/visualpasses/%d/41.702/-76.014/100/1/300/&apiKey=%s", id, encoded)
+	url := fmt.Sprintf("https://api.n2yo.com/rest/v1/satellite/visualpasses/%d/41.702/-76.014/100/2/300/&apiKey=%s", id, encoded)
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal("Error requesting Sat data:", err)
@@ -52,6 +53,9 @@ func visualPass() {
 	}
 	fmt.Println("Satellite Name:", passInfo.Info.Satname)
 	fmt.Println()
+	if len(passInfo.Pass) == 0 {
+		fmt.Println("Satellite not visible from your location for next 2 days.")
+	}
 	for _, i := range passInfo.Pass {
 		fmt.Println("Starting Azimuth of Pass (degrees):", i.StartAz)
 		fmt.Println("Direction:", i.StartAzComp)
@@ -62,5 +66,10 @@ func visualPass() {
 		fmt.Println("Ending Azimuth of Pass (degrees):", i.EndAz)
 		fmt.Println("Direction at End of Pass:", i.EndAzComp)
 		fmt.Println(time.Unix(int64(i.EndUTC), 0))
+		if i.Mag == 100000 {
+			fmt.Println("Magnitude cannot be determined.")
+		} else {
+			fmt.Println("Apparent Magnitude:", i.Mag)
+		}
 	}
 }
